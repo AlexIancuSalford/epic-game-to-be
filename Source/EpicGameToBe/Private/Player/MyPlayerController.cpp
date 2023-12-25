fd88f371fe92f91b8034150d5f3de5f3827eaa98
Alex Iancu <a.f.iancu@edu.salford.ac.uk>
2023-12-25 01:47:41 +0000

    Add empty player controller and input

---
+++ Source/EpicGameToBe/Private/Player/MyPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright Alex Iancu
+
+
+#include "Player/MyPlayerController.h"
+
---
+++ Source/EpicGameToBe/Public/Player/MyPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Alex Iancu
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EPICGAMETOBE_API AMyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
