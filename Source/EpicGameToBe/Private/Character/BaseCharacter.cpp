e78c2e709477b40d5aac518a12d80c2b38be7634
Alex Iancu <a.f.iancu@edu.salford.ac.uk>
2023-12-25 00:14:51 +0000

    Add player and goblin enemy blueprints

--- Source/EpicGameToBe/Private/Character/BaseCharacter.cpp
+++ Source/EpicGameToBe/Private/Character/BaseCharacter.cpp
@@ -7,6 +7,9 @@ ABaseCharacter::ABaseCharacter()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
