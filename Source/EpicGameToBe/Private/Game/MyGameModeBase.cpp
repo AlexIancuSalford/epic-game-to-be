58d66502e4c22baa694a9ce60e0a4a9ca88ced7c
Alex Iancu <a.f.iancu@edu.salford.ac.uk>
2023-12-25 13:13:30 +0000

    Add player camera and spring arm

---
+++ Source/EpicGameToBe/Private/Game/MyGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Alex Iancu
+
+
+#include "Game/MyGameModeBase.h"
+
---
+++ Source/EpicGameToBe/Public/Game/MyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Alex Iancu
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EPICGAMETOBE_API AMyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
