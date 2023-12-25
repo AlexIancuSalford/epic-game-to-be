6c19102b016155b7e986baa8f557d2dffb13dff8
Alex Iancu <a.f.iancu@edu.salford.ac.uk>
2023-12-25 02:37:16 +0000

    Add player controller move function and blueprint

--- Source/EpicGameToBe/Public/Player/MyPlayerController.h
+++ Source/EpicGameToBe/Public/Player/MyPlayerController.h
@@ -6,6 +6,11 @@
 #include "GameFramework/PlayerController.h"
 #include "MyPlayerController.generated.h"
 
+// Forward declare
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 /**
  * 
  */
@@ -14,4 +19,19 @@ class EPICGAMETOBE_API AMyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	AMyPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	void Move(const FInputActionValue& InputActionValue);
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> InputContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
 };
