418f0eac7065e02d0b8f3a05f8710710ccd49b2c
Alex Iancu <a.f.iancu@edu.salford.ac.uk>
2023-12-25 13:24:43 +0000

    Add enemy interface

---
+++ Source/EpicGameToBe/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright Alex Iancu
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
--- Source/EpicGameToBe/Public/Character/EnemyCharacter.h
+++ Source/EpicGameToBe/Public/Character/EnemyCharacter.h
@@ -4,14 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "Character/BaseCharacter.h"
+#include "Interaction/EnemyInterface.h"
 #include "EnemyCharacter.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class EPICGAMETOBE_API AEnemyCharacter : public ABaseCharacter
+class EPICGAMETOBE_API AEnemyCharacter : public ABaseCharacter, public IEnemyInterface
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void HighlightActor() override;
+
+	virtual void UnHighlightActor() override;
 };
