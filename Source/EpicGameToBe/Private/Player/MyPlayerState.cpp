baf0120dc047b5be79fba40486b185b4cc71b9bf
Alex Iancu <a.f.iancu@edu.salford.ac.uk>
2023-12-27 03:31:29 +0000

    Create player state

---
+++ Source/EpicGameToBe/Private/Player/MyPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Alex Iancu
+
+
+#include "Player/MyPlayerState.h"
+
+AMyPlayerState::AMyPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/EpicGameToBe/Public/Player/MyPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright Alex Iancu
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "MyPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EPICGAMETOBE_API AMyPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AMyPlayerState();
+};
